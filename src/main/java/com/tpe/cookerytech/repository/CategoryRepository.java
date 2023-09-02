14d2532522427b455f4c93c909520b6ab90c1ee3
ozcamseyma <ozcamseyma@gmail.com>
2023-09-02 19:24:48 +0300

    update

--- src/main/java/com/tpe/cookerytech/repository/CategoryRepository.java
+++ src/main/java/com/tpe/cookerytech/repository/CategoryRepository.java
@@ -14,5 +14,4 @@ public interface CategoryRepository  extends JpaRepository<Category,Long>{
     List<Category> findByIsActive(boolean isActive);
     boolean existsByTitle(String title);
 
-    List<Category> findByIsActive(boolean isActive);
 }
