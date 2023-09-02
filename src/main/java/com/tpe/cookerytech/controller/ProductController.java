d44ff95b525d4e41c2f180028199efdcf2f29ca7
ozcamseyma <ozcamseyma@gmail.com>
2023-09-02 20:18:34 +0300

    productupdate

--- src/main/java/com/tpe/cookerytech/controller/ProductController.java
+++ src/main/java/com/tpe/cookerytech/controller/ProductController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("products")
+@RequestMapping("/products")
 public class ProductController {
 
     private final ProductService productService;
