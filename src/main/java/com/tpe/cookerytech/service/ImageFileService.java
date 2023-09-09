381fe12ea427e1bf019b644a9583b02459576280
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2023-09-09 16:40:53 +0300

    image modifier

--- src/main/java/com/tpe/cookerytech/service/ImageFileService.java
+++ src/main/java/com/tpe/cookerytech/service/ImageFileService.java
@@ -1,10 +1,12 @@
 package com.tpe.cookerytech.service;
 
 import com.tpe.cookerytech.domain.ImageFile;
+import com.tpe.cookerytech.domain.Model;
 import com.tpe.cookerytech.domain.Product;
 import com.tpe.cookerytech.exception.ResourceNotFoundException;
 import com.tpe.cookerytech.exception.message.ErrorMessage;
 import com.tpe.cookerytech.repository.ImageFileRepository;
+import com.tpe.cookerytech.repository.ModelRepository;
 import com.tpe.cookerytech.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -17,20 +19,20 @@ public class ImageFileService {
 
     private final ImageFileRepository imageFileRepository;
 
-    private final ProductRepository productRepository;
+    private final ModelRepository modelRepository;
 
-    public ImageFileService(ImageFileRepository imageFileRepository, ProductRepository productRepository) {
+    public ImageFileService(ImageFileRepository imageFileRepository, ModelRepository modelRepository) {
         this.imageFileRepository = imageFileRepository;
-        this.productRepository = productRepository;
+        this.modelRepository = modelRepository;
     }
 
 
-    public List<ImageFile> getProductImages(Long productId) {
+    public List<ImageFile> getProductImages(Long modelId) {
 
-        Product product = productRepository.findById(productId).orElseThrow(()->
-                new ResourceNotFoundException(String.format(ErrorMessage.PRODUCT_NOT_FOUND_EXCEPTION,productId)));
+        Model model = modelRepository.findById(modelId).orElseThrow(()->
+                new ResourceNotFoundException(String.format(ErrorMessage.PRODUCT_NOT_FOUND_EXCEPTION,modelId)));
 
-        Set<ImageFile> images = product.getImage();
+        Set<ImageFile> images = model.getImage();
 
         // Resim setini bir liste olarak dönüştürün
         List<ImageFile> imageList = new ArrayList<>(images);
