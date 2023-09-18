76818e7c95a4d89a0765f247557f6200650262be
alper66 <63844547+alper66@users.noreply.github.com>
2023-09-18 15:37:54 -0400

    get details of an  offer

--- src/main/java/com/tpe/cookerytech/repository/OfferItemRepository.java
+++ src/main/java/com/tpe/cookerytech/repository/OfferItemRepository.java
@@ -4,8 +4,9 @@ import com.tpe.cookerytech.domain.OfferItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OfferItemRepository extends JpaRepository<OfferItem,Long> {
-
-
+    List<OfferItem> findByOfferId( Long id );
 }
